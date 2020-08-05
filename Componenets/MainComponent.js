bd6c11365362714200873d2d9b792f94427c0e29
ahsan-khan1999 <ahsan.ak943@gmail.com>
2020-08-05 11:03:40 +0500

    details rendering

--- Componenets/MainComponent.js
+++ Componenets/MainComponent.js
@@ -2,19 +2,27 @@ import React, { Component } from 'react'
 import {DISHES} from '../Shared/dishes'
 import MenuComponent from './MenuComponent'
 import {View} from 'react-native'
+import DishDetails from './DishDetails'
 class MainComponent extends Component {
 
     constructor(dishes){
         super(dishes)
         this.state = {
-            dishes : DISHES
+            dishes : DISHES,
+            setDish: null
         }
+    }
+    onDishSelect(dishId){
+        this.setState({setDish: dishId})
     }
 
     render() {
         return (
             <View>
-                <MenuComponent dishes={this.state.dishes} />
+                <MenuComponent dishes={this.state.dishes} 
+                onPress={(dishId) => this.onDishSelect(dishId)}
+                />
+                <DishDetails dish={this.state.dishes.filter((dish) => dish.id === this.state.setDish)[0]} />
             </View>
         )
     }
--- Componenets/MenuComponent.js
+++ Componenets/MenuComponent.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 import { View , FlatList } from 'react-native'
 import { ListItem } from 'react-native-elements'
 
-function MenuComponent({dishes}) {
-        
+function MenuComponent(props) {
+        const dishes = props.dishes
+        var name = props.dishes.name
     const renderItem = ({item,index}) => {
         return(
             <ListItem 
             key = {index}
             title = {item.name}
-            subtitle = {item.description}   
+            // subtitle = {item.description}
+            onPress = {() => props.onPress(item.id)}
             leftAvatar = {{source : require('./images/images/buffet.png')}} //javascript object
             />
         );
